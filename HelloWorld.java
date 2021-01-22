154ca8f114f277bf990e35f614af3810c2b46ede
jithin166 <jithinmurali166@gmail.com>
2021-01-22 23:06:28 +0530

    commit

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-import java.lang.System;
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
-//C:\Program Files\Java\jdk1.8.0_251\bin;
