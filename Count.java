5d6a7ea4f178012124b3071a7136b44cd94778b1
jithin166 <jithinmurali166@gmail.com>
2021-01-22 12:51:54 +0530

    commit

---
+++ Count.java
@@ -0,0 +1,10 @@
+public class Count {
+    public static void main(String[] args) {
+        int a=10;
+        for(int i=a;i>0;i--){
+            System.out.println(i);
+        }
+        
+    }
+    
+}
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
+import java.lang.System;
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
 }
+//C:\Program Files\Java\jdk1.8.0_251\bin;
